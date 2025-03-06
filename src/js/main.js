f2092759438a80222d840e4ae989b67659beb76e
mac <mac@MacBook-Pro-3.local>
2025-03-06 11:43:38 +0200

    added mega-menu logic to header

--- src/js/main.js
+++ src/js/main.js
@@ -19,6 +19,17 @@ function tabs() {
   });
 }
 
+function megaMenu() {
+  $('.header__nav li').on('mouseenter', function () {
+    $(this).find('.header__mega-menu').addClass('active');
+  }).on('mouseleave', function () {
+    $(this).find('.header__mega-menu').removeClass('active');
+  });
+  
+}
+
+
+
 function headerFixed(){
   const header = document.querySelector(".header");
   const links = document.querySelectorAll("a[href^='#']");
